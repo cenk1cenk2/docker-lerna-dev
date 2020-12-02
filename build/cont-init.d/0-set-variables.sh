f33cf503d92f233842e4c001432a3b6d2cf478bc
Cenk Kılıç <cenk@kilic.dev>
2020-12-03 00:36:54 +0100

    Update 0-set-variables.sh

--- build/cont-init.d/0-set-variables.sh
+++ build/cont-init.d/0-set-variables.sh
@@ -13,5 +13,5 @@ echo "DEBUG_PORT=${DEBUG_PORT_START:-9229}" >>/.env
 log_finish "Container variables generated."
 
 log_this "node: [$(node -v)]" "${CYAN}VERSION${RESET}"
-log_this "npm: [$(npm -v)]" "${CYAN}VERSION${RESET}"
+log_this "npm:  [$(npm -v)]" "${CYAN}VERSION${RESET}"
 log_this "yarn: [$(yarn -v)]" "${CYAN}VERSION${RESET}"
