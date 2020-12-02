44b6c9eddf3d86deb0b5cfadf7f351cedade6adc
Cenk Kılıç <cenk@kilic.dev>
2020-12-03 00:57:04 +0100

    add eval to execute and run in shell

--- build/cont-init.d/30-execute-commands.sh
+++ build/cont-init.d/30-execute-commands.sh
@@ -4,6 +4,6 @@ source /scripts/logger.sh
 
 if [ ! -z "${INIT_ENV_COMMAND}" ]; then
   log_start "\$ ${INIT_ENV_COMMAND}"
-  eval ${INIT_ENV_COMMAND}
+  eval "/bin/ash -c '${INIT_ENV_COMMAND}'"
   log_finish "\$ ${INIT_ENV_COMMAND}"
 fi
